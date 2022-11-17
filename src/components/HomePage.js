45f9056bde20a42bd912bfe3183db92f45cb9242
it19075204 <it19075204@my.sliit.lk>
2022-11-17 14:59:41 +0530

    added routes

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+            <h1>This is the homepage</h1>
+            <h1>This is the homepage </h1> 
+            </div>
+        );
+    }
+}
+
+
+export default HomePage;
